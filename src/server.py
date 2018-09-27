d9fd2c6e1e6babb34f38e5c8f5ba600148d2171f
Samuel Meuli <me@samuelmeuli.com>
2018-09-27 19:41:59 +0200

    Add authentication routes
    Co-authored-by: J-Klass <j-klass@hotmail.com>

--- src/server.py
+++ src/server.py
@@ -1,7 +1,24 @@
-from flask import Flask
+import json
+import os
 
+from flask import Flask, redirect, request
+from stravalib import Client
 
 app = Flask(__name__)
+client = Client()
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+if "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] == "development":
+    BASE_URL = "http://localhost:3000"
+else:
+    BASE_URL = "http://localhost:3000"  # TODO
+
+
+with open(os.path.join(os.path.dirname(current_dir), "config.json")) as config_file:
+    config = json.load(config_file)
+    CLIENT_ID = config["clientId"]
+    CLIENT_SECRET = config["clientSecret"]
 
 
 @app.route("/")
@@ -9,5 +26,25 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/auth-new", methods=["GET"])
+def get_auth_url():
+    redirect_uri = BASE_URL + "/auth-redirect"
+    url = client.authorization_url(client_id=CLIENT_ID, scope="write", redirect_uri=redirect_uri)
+    return redirect(url, code=302)
+
+
+@app.route("/auth-redirect", methods=["GET"])
+def parse_tokens():
+    code = request.args.get("code")
+    access_token = client.exchange_code_for_token(
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=code
+    )
+    print(access_token)
+    return redirect("/", code=302)
+
+
 if __name__ == "__main__":
-    app.run()
+    if "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] == "development":
+        app.run(host="0.0.0.0", port=3000)
+    else:
+        app.run()
