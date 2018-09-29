159f214d9ed6b0b72271d9ee249275a6e0cc7b65
Samuel Meuli <me@samuelmeuli.com>
2018-09-29 20:43:10 +0200

    Serve static files on all routes

--- src/server.py
+++ src/server.py
@@ -1,15 +1,22 @@
 import json
 import os
 
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, send_from_directory
 from stravalib import Client
 
 app = Flask(__name__)
+is_dev = "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] == "development"
+
+# Strava API client
 client = Client()
+
+# Directories
 current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+static_dir = os.path.join(parent_dir, "dist")
 
 
-if "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] == "development":
+if is_dev:
     BASE_URL = "http://localhost:3000"
 else:
     BASE_URL = "http://localhost:3000"  # TODO
@@ -19,11 +26,6 @@ with open(os.path.join(os.path.dirname(current_dir), "config.json")) as config_f
     config = json.load(config_file)
     CLIENT_ID = config["clientId"]
     CLIENT_SECRET = config["clientSecret"]
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 
 @app.route("/auth-new", methods=["GET"])
@@ -43,8 +45,20 @@ def parse_tokens():
     return redirect("/", code=302)
 
 
+# Serve static files in development mode (handled by nginx in production)
+if is_dev:
+
+    @app.route("/", methods=["GET"])
+    def get_html():
+        return send_from_directory(static_dir, "index.html")
+
+    @app.route("/<path:path>")
+    def catch_all(path):
+        return send_from_directory(static_dir, path)
+
+
 if __name__ == "__main__":
-    if "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] == "development":
-        app.run(host="0.0.0.0", port=3000)
+    if is_dev:
+        app.run(debug=True, host="0.0.0.0", port=3000)
     else:
         app.run()
