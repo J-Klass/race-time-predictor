5c1e785372f4cd81b7d0eb1e000c9de6f51b5829
Samuel Meuli <me@samuelmeuli.com>
2018-10-31 20:15:02 +0100

    Add header, fetch predictions on Athlete page

--- src/client/components/Header.vue
+++ src/client/components/Header.vue
@@ -3,3 +3,16 @@
 		<h1>Race Time Predictor</h1>
 	</header>
 </template>
+
+<style scoped>
+	header {
+		padding: var(--spacing-abs-small);
+		background: var(--color-background-hero);
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 140%;
+		font-weight: var(--font-weight-semibold);
+	}
+</style>
--- src/client/components/home/Hero.vue
+++ src/client/components/home/Hero.vue
@@ -40,17 +40,14 @@
 	}
 
 	h1 {
-		font-weight: var(--font-weight-bold);
 		letter-spacing: 0.05em;
+		font-weight: var(--font-weight-bold);
+		color: var(--color-strava);
 	}
 
 	.tagline {
 		font-weight: var(--font-weight-semibold);
 		letter-spacing: 0.025em;
-	}
-
-	h1 {
-		color: var(--color-strava);
 	}
 
 	@media (max-width: 850px) {
